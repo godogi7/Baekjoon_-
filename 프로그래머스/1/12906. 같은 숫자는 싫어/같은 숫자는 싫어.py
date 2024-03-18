1231821c39dab9f712e8bd00eba5e914556c156a
dongbin <70207640+godogi7@users.noreply.github.com>
2024-03-19 00:26:51 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 101.46 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,4 +1,4 @@
-
+"""
 def solution(arr):
     # 초기값 설정
     answer = [arr[0]]
@@ -6,5 +6,40 @@ def solution(arr):
         if answer[-1] != arr[i]:
             answer.append(arr[i])
     return answer
+"""
+# 넣으려는 값이 가장 끝 값과 다르면 푸시
+"""
+1. 스
+"""
+def solution(arr):
+    answer = []
+    # 스택[0] = 배열[0]
+    # answer[0] = arr[0]
+    # i > 0,1,2,3,4
+    for i in range(len(arr)):
+        if i == 0:
+            answer.append(arr[i])
+        else:
+            if answer[-1] != arr[i]:
+                answer.append(arr[i])
+            else:
+                pass
+    
+    return answer
 
-#넣으려는 값이 가장 끝 값과 다르면 푸시
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
